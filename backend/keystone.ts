aebe387e0842fded50dd80c66efa33e55ca3aee5
Luke Miller <lukem1293@gmail.com>
2021-02-09 15:02:39 -0600

    many changes

---
+++ backend/keystone.ts
@@ -0,0 +1,63 @@
+import { createAuth } from "@keystone-next/auth";
+import { config, createSchema } from "@keystone-next/keystone/schema";
+import {
+  statelessSessions,
+  withItemData,
+} from "@keystone-next/keystone/session";
+import "dotenv/config";
+import { Product } from "./schemas/Product";
+import { ProductImage } from "./schemas/ProductImage";
+import { User } from "./schemas/User";
+import { insertSeedData } from "./seed-data";
+const databaseURL =
+  process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits-tutorial";
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 30 * 360, // how long should they stay signed in
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
+  initFirstItem: {
+    fields: ["name", "email", "password"],
+    // TODO: Add in inital roles here
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: "mongoose",
+      url: databaseURL,
+      async onConnect(keystone) {
+        if (process.argv.includes("--seed-data")) {
+          await insertSeedData(keystone);
+        }
+      },
+    },
+    lists: createSchema({
+      // schema items go in here
+      User,
+      Product,
+      ProductImage,
+    }),
+    ui: {
+      // Show the UI only for people who pass this test
+      isAccessAllowed: ({ session }) => {
+        return !!session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: `id`,
+    }),
+  })
+);
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,11 +1,33 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
+import { ApolloProvider } from '@apollo/client';
+import Router from 'next/router';
+import Nprogress from 'nprogress';
 import Page from '../components/Page';
+import '../components/styles/nprogress.css';
+import withData from '../lib/withData';
 
-export default function MyApp({ Component, pageProps }) {
+Router.events.on('routeChangeStart', () => Nprogress.start());
+Router.events.on('routeChangeComplete', () => Nprogress.done());
+Router.events.on('routeChangeError', () => Nprogress.done());
+
+function MyApp({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return pageProps;
+};
+
+export default withData(MyApp);
