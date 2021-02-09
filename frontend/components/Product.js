731607c0a566baaacb4f9280c3eef966d9c5dc4e
Luke Miller <lukem1293@gmail.com>
2021-02-09 15:34:44 -0600

    product component

---
+++ frontend/components/Product.js
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+import Link from 'next/link';
+import formatMoney from '../lib/formatMoney';
+import ItemStyles from './styles/ItemStyles';
+import PriceTag from './styles/PriceTag';
+import Title from './styles/Title';
+
+export default function Product({ product }) {
+  return (
+    <ItemStyles>
+      <img
+        src={product?.photo?.image?.publicUrlTransformed}
+        alt={product?.name}
+      />
+      <Title>
+        <Link href={`/product/${product.id}`}>{product?.name}</Link>
+      </Title>
+      <PriceTag>{formatMoney(product.price)}</PriceTag>
+      <p>{product?.description}</p>
+      {/** TODO: Add butons to edit and delete item */}
+    </ItemStyles>
+  );
+}
--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -1,5 +1,7 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
+import styled from 'styled-components';
+import Product from './Product';
 
 const ALL_PRODUCTS_QUERY = gql`
   query ALL_PRODUCTS_QUERY {
@@ -7,6 +9,7 @@ const ALL_PRODUCTS_QUERY = gql`
       id
       name
       description
+      price
       photo {
         id
         altText
@@ -18,12 +21,25 @@ const ALL_PRODUCTS_QUERY = gql`
   }
 `;
 
+const ProductsListStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+`;
+
 export default function Products() {
   const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
   console.log(data, error, loading);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error?.message}</p>;
   return (
     <div>
-      <p>Products!</p>
+      <ProductsListStyles>
+        {data.allProducts.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </ProductsListStyles>
     </div>
   );
 }
